3dd8de3d3c78bab042f50257059824fb0642a7cc
codegr33n <greenbrightsmart@gmail.com>
2023-05-18 14:12:15 +0100

    javascript fundamentals

---
+++ app.js
@@ -0,0 +1,104 @@
+// printing to the console
+// console.log('Helo JS');
+
+// Data Types
+
+// 1. Primitive Types
+
+// Primitives are values that can only be of a single data type
+
+// parse => read, interpret
+
+/**
+ * 1.  strings - regular text
+ * 2. numbers - numbers, including floats and ints
+ * 3. boolean - true, or false
+ * 4. null - empty value
+ * 5. undefined -
+ * 6. symbols - unique values
+ * 7. BigInt - represent values that cannt be represented in memory
+ */
+
+// Strings:
+
+// 1. single quotes = ''
+// 2. double quotes = ""
+// 3. Template literals = ``
+
+// values
+
+// variables
+//  constants
+
+// 1. var keyword
+//  2. let keyword
+// 3. using the name of the variable itself
+
+// escape character = \ - backslash
+
+// var storeName = "Comfort's Store";
+// let myName = 'Bright';
+
+// variable declaration
+// let userName;
+
+// initialization - assign a value to the variable
+// userName = 'Mayowa';
+
+// combine the two steps
+// let userName = '';
+
+// Re-assigning
+// userName = 'user1';
+
+// console.log(userName)
+
+// 1. variable declaration
+// 2. variable initialization
+
+// console.log(storeName);
+
+// 2.  Reference Types
+// Reference are values that can contain multiple data-types at a time
+
+/**
+ * 1. objects
+ * 2. arrays
+ * 3. maps
+ * 4. sets
+ * 5. functions
+ */
+
+// let userName, myName, score, age;
+
+// userName = 'user1';
+// myName = 'Bright';
+// score = 100;
+// age = 100;
+// userName = 'user1';
+// myName = 'Bright';
+// score = 100;
+// age = 100;
+
+// console.log(userName, myName, score, age);
+
+// syntax
+
+// constants
+
+// const PI = 3.14;
+// variable name = identifiers
+
+// 3. Template literals = `` es6 standard 2015
+
+
+let reciever = 'Mayowa'
+let msgToMayoway = `Hello, what's up ${reciever}`;
+
+
+console.log(msgToMayoway);
+// interpolation
+
+
+// expression - a javascript code that evaluates to produce a value
+
